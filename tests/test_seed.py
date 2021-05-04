f024b513cbe24da6d16189f34959cf02f13d9e3a
Jen Brissman <brissman514@gmail.com>
2021-05-04 11:21:27 -0700

    adding test_seed.py

---
+++ tests/test_seed.py
@@ -0,0 +1,20 @@
+from sqlalchemy import func
+from model import User, Image, db
+from server import app
+
+
+def test_user():
+    """Creates test user in test database"""
+
+    test_user = User(first_name = 'Jenny', last_name = 'Testman', email = 'test@test.com', password = 'shopify')
+    db.session.add(test_user)
+    db.session.commit()
+
+
+def test_image():
+    """Creates test image in test database"""
+
+    test_image = Image(user_id=1, image_title = "Sailor's Kiss", artist = "Kobra", location = "The High Line, West 30th Street, New York, NY, USA", image_url="http://res.cloudinary.com/jenbrissman/image/upload/v1619933391/meq168zr4pglkskc8oge.jpg")
+    db.session.add(test_image)
+    db.session.commit()
+    
